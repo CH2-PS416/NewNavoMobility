e8e743be1f8f6e7d2649256fcb5d50f738cf7979
jidan18jidan <jidan18jidan@apps.ipb.ac.id>
2023-12-22 16:53:49 +0700

    recreate.log in.

--- app/src/main/java/com/bangkit/navomobility/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/bangkit/navomobility/ui/screen/home/HomeScreen.kt
@@ -1,37 +1,107 @@
-package com.bangkit.navomobility.ui.screen.home
+package com.bangkit.navomobility.ui.screen.history
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+// Dummy data class representing a tourist attraction
+data class TouristAttraction(val name: String, val location: String)
+
 @Composable
-fun HomeScreen() {
+fun TouristAttractionCard(attraction: TouristAttraction) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(MaterialTheme.shapes.medium)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = attraction.name,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = attraction.location,
+                fontSize = 16.sp,
+                color = LocalContentColor.current.copy(alpha = 0.8f)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistoryScreen() {
+    var searchText by remember { mutableStateOf("") }
+
+    val recommendedAttractions = listOf(
+        TouristAttraction("Attraction A", "Location A"),
+        TouristAttraction("Attraction B", "Location B"),
+        // Add more attractions as needed
+    )
+
+    val filteredAttractions = recommendedAttractions.filter {
+        it.name.contains(searchText, ignoreCase = true) ||
+                it.location.contains(searchText, ignoreCase = true)
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Magenta),
-        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "HOME",
-            fontSize = 24.sp, // Ganti dengan ukuran yang diinginkan
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Column {
+            // Search Bar
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                label = { Text("Search Attractions") },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                singleLine = true
+            )
 
+            // Recommended Attractions
+            LazyColumn(
+                state = rememberLazyListState(),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(filteredAttractions) { attraction ->
+                    TouristAttractionCard(attraction = attraction)
+                }
+            }
+        }
     }
 }
 
-@Composable
-@Preview
-fun HomeScreenPreview() {
-    HomeScreen()
-}
+
